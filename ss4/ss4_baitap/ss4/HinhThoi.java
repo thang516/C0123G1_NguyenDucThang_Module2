1c421382c5119420ad2296a8810f747f00fbcf69
thang516 <nguyenthangfa2001@gmail.com>
2023-03-09 22:05:47 +0700

    baitapss4

--- ss4/ss4_baitap/ss4/HinhThoi.java
+++ ss4/ss4_baitap/ss4/HinhThoi.java
@@ -2,32 +2,31 @@ package ss4;
 
 public class HinhThoi {
     public static void main(String[] args) {
-         int  middle = 5;
-         int left=4;
-         int right=5;
-         int index =  1;
+        int left = 4;
+        int right = 5;
+        int index = 1;
         for (int i = 1; i <= 9; i++) {
             for (int j = 0; j < 10; j++) {
-               if(j==left || j==right){
-                   System.out.print(index);
-               }else{
-                   System.out.print(" ");
-               }
+                if (j == left || j == right) {
+                    System.out.print(index);
+                } else {
+                    System.out.print(" ");
+                }
             }
-            if(i>4){
+            if (i > 4) {
                 index--;
                 left++;
                 right--;
-            }else{
+            } else {
                 index++;
                 left--;
                 right++;
 
             }
-        System.out.println();
-        }
+            System.out.println();
         }
     }
+}
 
 
 
