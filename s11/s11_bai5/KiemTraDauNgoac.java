7b89fdf91441e4e8dfec9600080de0f3c64a4528
thang516 <nguyenthangfa2001@gmail.com>
2023-03-17 20:41:16 +0700

    baitapss11

--- s11/s11_bai5/KiemTraDauNgoac.java
+++ s11/s11_bai5/KiemTraDauNgoac.java
@@ -4,33 +4,27 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class KiemTraDauNgoac {
-   public  static boolean kiemTraDauNgoac(String str){
-    Stack<Character> stack=new Stack<>();
-    char[] arr= str.toCharArray();
-       for (char x: arr) {
-           if (x == '('){
-               stack.push(x);
-           } else if (x == ')') {
-               if(stack.isEmpty()){
-                   return false;
-               }
-               char last = stack.peek();
-               if((x == '(' && last ==')')){
-                   stack.pop();
-               }else{
-                   return false;
-               }
-           }
-
-       }
-       return stack.isEmpty();
-   }
+    public static boolean kiemTraDauNgoac(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = str.toCharArray();
+        for (char x : arr) {
+            if (x == '(') {
+                stack.push(x);
+            } else if (x == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println(" Nhập  vô 1 chuỗi bâất kì có chứa ngoặc ");
-        String str=scanner.nextLine();
-        System.out.println(str +"Sau khi đã biến đổi"+ kiemTraDauNgoac(str));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Nhập  vô 1 chuỗi bất kì có chứa ngoặc ");
+        String str = scanner.nextLine();
+        System.out.println(str + "Sau khi kiểm tra : " + kiemTraDauNgoac(str));
     }
 
 }
