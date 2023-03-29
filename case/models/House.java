6cd655ba8f55b8bb67e32388e3f25e28bb899276
thang516 <nguyenthangfa2001@gmail.com>
2023-03-29 19:02:51 +0700

    baitap

---
+++ case/models/House.java
@@ -0,0 +1,52 @@
+package models;
+
+public class Hourse extends Facility{
+  private String roomStandard;
+   private String numberOfFloor;
+
+    public Hourse(String nameFacility, String usableArea, String rentalCosts, String maximumQuantity, String typeOfRental, String roomStandard, String numberOfFloor) {
+        super(nameFacility, usableArea, rentalCosts, maximumQuantity, typeOfRental);
+        this.roomStandard = roomStandard;
+        this.numberOfFloor = numberOfFloor;
+    }
+
+    public Hourse(Facility f, String roomStandard, String numberOfFloor) {
+        super(f.nameFacility, f.usableArea, f.rentalCosts, f.maximumQuantity, f.typeOfRental);
+        this.roomStandard = roomStandard;
+        this.numberOfFloor = numberOfFloor;
+    }
+
+    public Hourse() {
+    }
+
+    public String getRoomStandard() {
+        return roomStandard;
+    }
+
+    public void setRoomStandard(String roomStandard) {
+        this.roomStandard = roomStandard;
+    }
+
+    public String getNumberOfFloor() {
+        return numberOfFloor;
+    }
+
+    public void setNumberOfFloor(String numberOfFloor) {
+        this.numberOfFloor = numberOfFloor;
+    }
+
+    @Override
+    public String toString() {
+        return "Hourse :" +
+                ", Tên dịch vụ : " + this.getNameFacility() + '\'' +
+                ", Diện tích sử dụng : " + this.getUsableArea() +
+                ", Chi phí thuê :" + this.getRentalCosts() +
+                ", Số lượng tối đa : " + this.getMaximumQuantity() +
+                ", Kiểu thuê :  " + this.getTypeOfRental()+
+                ", Tiêu chuẩn phòng :" + this.getRoomStandard() + '\'' +
+                ", Số tầng :" + this.getNumberOfFloor() ;
+    }
+    public String getInfoToCsv(){
+        return nameFacility+","+usableArea+","+rentalCosts+","+maximumQuantity+","+typeOfRental+","+roomStandard+","+numberOfFloor;
+    }
+}
